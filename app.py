b3813f6f92a16e0dda3f1d420c9bf296db694f0f
yaninaslab <yana.belash@gmail.com>
2022-02-14 23:31:53 -0500

    Added API requests

---
+++ app.py
@@ -0,0 +1,47 @@
+
+from sre_constants import SUCCESS
+from flask import Flask, request, Response
+import json
+
+app = Flask(__name__)
+
+
+@app.get('/animals')
+def list_animals():
+    # Defining a list of items
+    animals = ["dog", "cat", "lion", "snake", "owl"]
+    # Converting them to json
+    animals_json = json.dumps(animals, default=str)
+    # Getting the response in json
+    return Response(animals_json, mimetype="application/json", status=200)
+
+
+@app.post('/animals')
+def add_animal():
+    animals = ["dog", "cat", "lion", "snake", "owl"]
+    # Adding item to the end of the list
+    animals.append("snake")
+    animals_json = json.dumps(animals, default=str)
+    return Response(animals_json, mimetype="application/json", status=200)
+
+
+@app.patch('/animals')
+def edit_animal():
+    animals = ["dog", "cat", "lion", "snake", "owl"]
+    # Assigning new value to the list's item
+    if "owl" in animals:
+        animals[4] = "snowy_owl"
+        animals_json = json.dumps(animals, default=str)
+        return Response(animals_json, mimetype="application/json", status=200)
+
+
+@app.delete('/animals')
+def del_animal():
+    animals = ["dog", "cat", "lion", "snake", "owl"]
+    # Removes the last element of the list if the index is not specified
+    animals.pop()
+    animals_json = json.dumps(animals, default=str)
+    return Response(animals_json, mimetype="application/json", status=200)
+
+
+app.run(debug=True)
